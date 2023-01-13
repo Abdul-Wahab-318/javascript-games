54579e370a04d01d19df07580890fbfd9e22d151
Abdul Wahab Malik <wahabmaliq@gmail.com>
2023-01-13 16:56:12 +0500

    adds more funcyions in breakout game

--- 04-breakout/script.js
+++ 04-breakout/script.js
@@ -1,10 +1,18 @@
 let container = document.getElementById("container")
 let containerWidth = container.offsetWidth
 let containerHeight = container.offsetHeight
+
 let score = 0 
+const blocks = []
 const blockHeight = 20
 const blockWidth = 120
-const blocks = []
+
+
+const ballDiamter = 12
+let ballStart = [ 385 , 85 ] ; 
+let ballCurrentPos = ballStart
+let xdirection = 2 // direction in which ball is moving
+let ydirection = 2 
 
 let userStartPos = [340 , 60]
 let currentPos = userStartPos
@@ -62,12 +70,22 @@ function drawBlocks()
 fillBlockArray()
 drawBlocks()
 
+function drawBall()
+{
+    ball.style.left = ballCurrentPos[0] + 'px'
+    ball.style.bottom = ballCurrentPos[1] + 'px'
+}
 
 let user = document.createElement("div")
 user.classList.add('user')
 user.style.left = currentPos[0] + 'px'
 user.style.bottom = currentPos[1] + 'px'
 container.appendChild( user )
+
+const ball = document.createElement('div') 
+ball.classList.add('ball')
+drawBall()
+container.appendChild(ball)
 
 window.addEventListener('keydown' , moveUser )
 
@@ -101,6 +119,38 @@ function moveUser( event )
     }
 }
 
+function moveBall()
+{
+    ballCurrentPos[0] += 2 
+    ballCurrentPos[1] += 2 
+    drawBall()
+    checkForCollision()
+}
+
+function checkForCollision()
+{
+    if ( 
+        ballCurrentPos[0] >= ( containerWidth - ballDiamter ) ||
+        ballCurrentPos[1] >= ( containerHeight - ballDiamter )
+    )
+    {
+        changeDirection()
+    }
+}
+
+function changeDirection()
+{
+    if ( xdirection === 2 && ydirection === 2 )
+        ydirection = -2 
+    else if ( xdirection === 2 && ydirection === -2 )
+        xdirection = -2
+    else if ( xdirection === -2 && ydirection === -2 )
+        ydirection = 2 
+    else if ( xdirection === -2 && ydirection === 2)
+        xdirection = 2  
+}
+
+let movingBallInterval = setInterval( moveBall , 10)
 
 
 
